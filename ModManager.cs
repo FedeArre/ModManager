47dc510750fd9ccb83368b1a97f8293e15f0ff50
Federico <federicoarredondoc@gmail.com>
2021-11-01 17:46:59 -0300

    Add project files.

---
+++ ModManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ModManager
+{
+    public class ModManager : Mod
+    {
+        public override string ID => "ModManager";
+        public override string Name => "ModManager";
+        public override string Author => "Federico Arredondo";
+        public override string Version => "1.0.0";
+    }
+}
