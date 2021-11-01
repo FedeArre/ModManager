e8679a5ad36f51993b829a340625e6164918ded5
Federico <federicoarredondoc@gmail.com>
2021-11-01 18:16:35 -0300

    feat(mod-register): mod can be registered to the manager through ModManager.RegisterMod

--- ModManager.cs
+++ ModManager.cs
@@ -10,5 +10,14 @@ namespace ModManager
         public override string Name => "ModManager";
         public override string Author => "Federico Arredondo";
         public override string Version => "1.0.0";
+
+        internal static List<ModSettings> RegisteredMods = new List<ModSettings>();
+
+        public static ModSettings RegisterMod(string modId)
+        {
+            ModSettings ms = new ModSettings(modId);
+            RegisteredMods.Add(ms);
+            return ms;
+        }
     }
 }
---
+++ ModSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModManager
+{
+    public class ModSettings
+    {
+        private string modId;
+
+        public ModSettings(string modId)
+        {
+            this.modId = modId;
+        }
+    }
+}
