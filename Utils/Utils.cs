bb4e65f701bfeeb780e9227a93d13e4bec519ceb
Federico <federicoarredondoc@gmail.com>
2021-11-06 15:34:24 -0300

    feat: created utils with mods and mod settings folders path

---
+++ Utils/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ModManager
+{
+    public class Utils
+    {
+        public static string MODS_FOLDER_PATH = Application.dataPath + "/../Mods";
+        public static string MODS_SETTINGS_FOLDER_PATH = MODS_FOLDER_PATH + "/modSettings";
+
+    }
+}
