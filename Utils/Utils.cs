af17cde33ae83dcc32dd39368a9559ff5f6637ac
Federico <federicoarredondoc@gmail.com>
2021-11-06 16:16:55 -0300

    feat: created GetSettingByIdInList

--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -12,5 +12,15 @@ namespace ModManager
         public static string MODS_FOLDER_PATH = Application.dataPath + "/../Mods";
         public static string MODS_SETTINGS_FOLDER_PATH = MODS_FOLDER_PATH + "/modSettings";
 
+        public static Settings GetSettingByIdInList(List<Settings> list, string id)
+        {
+            foreach(Settings s in list)
+            {
+                if (s.id == id)
+                    return s;
+            }
+
+            return null;
+        }
     }
 }
